5f12e602cc721966b1246d7fda9703de3e336f9a
khaled <khaledb.yahya@gmail.com>
2022-07-26 20:38:14 +0100

    form validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,48 @@
-import React from 'react';
-
+import React from "react";
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-8'>
-        <form method='POST' action="https://getform.io/f/65c92a0f-7a6c-4355-83a7-dcd78c5a552f" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-2'>
-                <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - khaledb.yahya@gmail.com </p>
-            </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="5" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-blue-600 hover:border-blue-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
-        </form>
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-8"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/65c92a0f-7a6c-4355-83a7-dcd78c5a552f"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-2">
+          <p className="text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">Submit the form below</p>
+        </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+          required
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="5"
+          placeholder="Message"
+          required
+        ></textarea>
+        <button className="text-white border-2 hover:bg-blue-600 hover:border-blue-600 px-4 py-3 my-8 mx-auto flex items-center">
+          Let's Collaborate
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
