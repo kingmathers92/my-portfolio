290747f0ee286eadf32674467c9b1ecb66bac1e7
khaled <khaledb.yahya@gmail.com>
2023-01-06 18:44:36 +0100

    improved mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,11 +23,6 @@ const Navbar = ({ theme, setTheme }) => {
       </div>
 
       <menu className="hidden md:flex">
-        {/* <button id="switch-theme">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-          </svg>
-        </button> */}
         <DarkModeSwitch
           style={{ marginBottom: "0rem" }}
           checked={theme === "dark"}
@@ -72,9 +67,42 @@ const Navbar = ({ theme, setTheme }) => {
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+            : "absolute top-0 left-0 w-full h-screen mobileMenu flex flex-col justify-center items-center"
         }
       >
+        <div className="darkModeMobile">
+          <DarkModeSwitch
+            style={{ marginBottom: "0rem" }}
+            checked={theme === "dark"}
+            onChange={toggleDarkMode}
+            size={25}
+          />
+        </div>
+        <div className="socialsHidden">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/khaledbenyahya/"
+              target="blank"
+            >
+              <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/kingmathers92" target="blank">
+              <FaGithub size={30} />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:khaledb.yahya@gmail.com" target="blank">
+              <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li>
+            <a href={require("../assets/resume.pdf")} download="Resume">
+              <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </div>
         <li className="py-6 text-4xl no-underline hover:underline">
           <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -24,15 +24,15 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
             <p className="my-4">CSS</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img
               className="w-20 mx-auto"
               src={JavaScript}
@@ -40,31 +40,31 @@ const Skills = () => {
             />
             <p className="my-4">JavaScript</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
             <p className="my-4">React</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Node} alt="Node icon" />
             <p className="my-4">Node</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={GitHub} alt="Github icon" />
             <p className="my-4">Github</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Bootstrap} alt="Tailwind icon" />
             <p className="my-4">Bootstrap</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Django} alt="Django icon" />
             <p className="my-4">Django</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Python} alt="Django icon" />
             <p className="my-4">Python</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+          <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Tailwind} alt="Django icon" />
             <p className="my-4">Tailwind CSS</p>
           </div>
