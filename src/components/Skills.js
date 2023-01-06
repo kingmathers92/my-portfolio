9daa53565e69d83ccc94ff220b5ed703e3db73b5
khaled <khaledb.yahya@gmail.com>
2023-01-06 19:00:09 +0100

    styled skill imgs

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -25,15 +25,26 @@ const Skills = () => {
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={HTML}
+              alt="HTML icon"
+            />
             <p className="my-4">HTML</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={CSS}
+              alt="CSS icon"
+            />
             <p className="my-4">CSS</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img
+              style={{ padding: "10px" }}
               className="w-20 mx-auto"
               src={JavaScript}
               alt="Javascript icon"
@@ -41,31 +52,66 @@ const Skills = () => {
             <p className="my-4">JavaScript</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={ReactImg}
+              alt="React icon"
+            />
             <p className="my-4">React</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Node} alt="Node icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={Node}
+              alt="Node icon"
+            />
             <p className="my-4">Node</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={GitHub} alt="Github icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={GitHub}
+              alt="Github icon"
+            />
             <p className="my-4">Github</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Bootstrap} alt="Tailwind icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={Bootstrap}
+              alt="Tailwind icon"
+            />
             <p className="my-4">Bootstrap</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Django} alt="Django icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={Django}
+              alt="Django icon"
+            />
             <p className="my-4">Django</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Python} alt="Django icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={Python}
+              alt="Django icon"
+            />
             <p className="my-4">Python</p>
           </div>
           <div className="skills shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Tailwind} alt="Django icon" />
+            <img
+              style={{ padding: "10px" }}
+              className="w-20 mx-auto"
+              src={Tailwind}
+              alt="Django icon"
+            />
             <p className="my-4">Tailwind CSS</p>
           </div>
         </div>
