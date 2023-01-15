aaa0ad2e568446cfa337fb1a68ef17cebbcd6967
khaled <khaledb.yahya@gmail.com>
2023-01-15 11:56:34 +0100

    added project

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,7 @@
 import weatherApp from "../assets/weatherpic.png";
 import lyricsFetcher from "../assets/lyricsfetcher.png";
 import travelPage from "../assets/travel.png";
+import tenzies from "../assets/tenzies.png";
 import pdfToAdudio from "../assets/logo-color.png";
 import crypto from "../assets/cryptod.png";
 
@@ -28,13 +29,20 @@ export const data = [
   },
   {
     id: 4,
+    name: "Tenzies Game",
+    image: tenzies,
+    github: "https://github.com/kingmathers92/React-Tenzies-Game",
+    live: "https://kingmathers92.github.io/React-Tenzies-Game/",
+  },
+  {
+    id: 5,
     name: "PDF To Audio",
     image: pdfToAdudio,
     github: "https://github.com/kingmathers92/pdfToAudio",
     live: "",
   },
   {
-    id: 5,
+    id: 6,
     name: "Crypto Monitor",
     image: crypto,
     github: "https://github.com/kingmathers92/CryptoMonitor",
