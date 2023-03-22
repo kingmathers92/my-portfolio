85d7da4a1505b516cea609809dcf8b0f6644b217
khaled <khaledb.yahya@gmail.com>
2023-03-22 19:56:54 +0100

    blog fixes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,39 +5,41 @@ import Pagination from "./Pagination.jsx";
 function Blog({ username }) {
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(6);
+  const itemsPerPage = 3;
+
+  const currentPosts = posts.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const totalPages = Math.ceil(posts.length / itemsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   useEffect(() => {
     const username = "@khaledb.yahya";
-    const url = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${username}`;
+    const url = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${username}}`;
     axios
       .get(url)
       .then((response) => {
         const posts = response.data.items;
         setPosts(posts);
-        setPostsPerPage(posts.length);
         console.log(posts);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, [username]);
+  }, []);
 
   const stripTags = (html) => {
     if (!html) return "";
     return html.replace(/<[^>]+>/g, "").replace(/Photo\s.*\sUnsplash/, "");
   };
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
   return (
-    <div name="blog" className="w-full md:h-screen py-16">
+    <div name="blog" className="w-full md:h-screen mt-20">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-blue-600">
@@ -49,7 +51,7 @@ function Blog({ username }) {
           {currentPosts.map((post) => (
             <div
               key={post.guid}
-              className="rounded-lg group container mx-auto shadow-sm shadow-blue-300 m-2 p-2"
+              className="rounded-lg group h-auto mx-auto shadow-sm shadow-blue-300 m-2 p-2"
             >
               <img src={post.thumbnail} alt="post" />
               <a href={post.link} target="_blank" rel="noreferrer">
@@ -57,18 +59,20 @@ function Blog({ username }) {
                   {post.title}
                 </p>
                 <br />
-                <p className="text-sm font-light">
-                  {stripTags(post.description).slice(0, 250)}
-                  {stripTags(post.description).length > 250 ? "..." : ""}
-                </p>
+                <div>
+                  <p className="text-sm font-light">
+                    {stripTags(post.description).slice(0, 250)}
+                    {stripTags(post.description).length > 250 ? "..." : ""}
+                  </p>
+                </div>
               </a>
-              <div className="flex items-center mt-4">
+              <div className="flex items-center right-0 bottom-0">
                 <img
-                  src="https://imgtr.ee/images/2023/03/21/q3GYx.th.jpg"
+                  src="https://imgtr.ee/images/2023/03/22/qgGMU.png"
                   className="rounded-full w-12 h-12 my-2"
                   alt="face"
                 />
-                <p className="mx-2 flex flex-col-reverse ml-40">
+                <p className="mx-3">
                   <span className="text-gray-200 text-sm">
                     {new Date(post.pubDate).toLocaleDateString()}
                   </span>
@@ -76,12 +80,12 @@ function Blog({ username }) {
               </div>
             </div>
           ))}
-          <Pagination
-            postsPerPage={postsPerPage}
-            totalPosts={posts.length}
-            paginate={paginate}
-          />
         </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
       </div>
     </div>
   );
