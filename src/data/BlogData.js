2f36d3c328ff15acee2585d5d9caf89af7b28aa3
khaled <khaledb.yahya@gmail.com>
2023-03-23 19:51:08 +0100

    rss trial ended, hardcoded blog data instead

---
+++ src/data/BlogData.js
@@ -0,0 +1,27 @@
+export const blogData = [
+  {
+    title: "My way of learning..",
+    description:
+      "Lately, I’ve found I’ve been able to successfully make whatever I’m attempting in any language I’m trying to. Without failure. Am I smart?...",
+    image: "https://miro.medium.com/v2/resize:fit:720/0*RzQ3gktrKJKQTr4s",
+    time: "2 mins read",
+    link: "https://medium.com/@khaledb.yahya/lately-ive-found-i-ve-been-able-to-successfully-make-whatever-i-m-attempting-in-any-language-i-m-665289b8d254?source=user_profile---------2----------------------------",
+  },
+  {
+    title: "How to Effectively Remove .env File from Git Repo/History",
+    description:
+      "The .env file is typically located in the root directory of a project and is not committed to version control systems, such as Git...",
+    image: "https://miro.medium.com/v2/resize:fit:720/0*OCoBt-Nth9vji_Le",
+    time: "3 mins read",
+    link: "https://medium.com/@khaledb.yahya/how-to-effectively-remove-env-file-from-git-repo-history-bd5f14b7a322?source=user_profile---------1----------------------------",
+  },
+
+  {
+    title: "Redux for State Management",
+    description:
+      "There are times when developers get stuck with a problem that has to do with version changes. Trying to run the code or test without upgrading the package can result in unexpected errors...",
+    image: "https://miro.medium.com/v2/resize:fit:1400/0*IzXEwuhsVbIuY4vv",
+    time: "4 mins read",
+    link: "https://medium.com/@khaledb.yahya/redux-for-state-management-de286cd99270/",
+  },
+];
