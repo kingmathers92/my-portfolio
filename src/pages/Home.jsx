bfcaad0e91b87724b18ae7bc324db1ce36e0b7ba
khaled <khaledb.yahya@gmail.com>
2023-09-22 19:32:16 +0100

    fixed broken smooth scroll links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen pt-0 pb-0">
+    <div name="about" id="about" className="w-full h-screen pt-0 pb-0">
       {/* Container */}
       <div className="max-w-screen-lg mx-auto px-8 flex flex-col justify-center h-full">
         <p className="hi left-animate font-semibold">Hi, my name is</p>
@@ -14,7 +14,7 @@ const Home = () => {
           I'm a Software Developer.
         </h2>
         <p className="about text-[#8892b0] font-semibold py-4 max-w-screen-md right-animate">
-          I’m Software Developer specializing in building exceptional digital
+          I’m a Software Developer specializing in building exceptional digital
           experiences. Currently, I’m focused on building responsive full-stack
           web applications.
         </p>
