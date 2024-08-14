1b190be8e40016bef0c3b6718ca7cff40bc6769f
KHALED\E392 <khaledb.yahya@gmail.com>
2024-08-14 22:16:00 +0100

    Updates

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "portfolio-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/main.chunk.js",
+  "/static/js/0.chunk.js",
+  "/static/js/bundle.js",
+  "/static/css/main.chunk.css",
+];
+
+/* eslint-disable no-restricted-globals */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+/* eslint-disable no-restricted-globals */
