aa083635608cbe01d4d166e575016b085872e6f1
KHALED\E392 <khaledb.yahya@gmail.com>
2024-08-14 22:14:43 +0100

    seperate quote component

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as Pagination } from "./Pagination";
 export { default as ScrollBackToTop } from "./ScrollBackToTop";
 export { default as Project } from "./Project";
 export { default as AnimatedElement } from "./AnimatedElement";
+export { default as RandomQuote } from "./RandomQuote";
 export { default as ErrorBoundary } from "./ErrorBoundary";
