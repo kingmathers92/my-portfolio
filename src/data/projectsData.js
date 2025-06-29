2b3edbd14c8a6d0ebfa22ba3ff2c56da6b2fa1ff
khaled <khaledb.yahya@gmail.com>
2025-06-29 18:08:53 +0100

    removed project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,4 +1,3 @@
-import tenzies from "../assets/tenzies.png";
 import crypto from "../assets/cryptod.png";
 import moviehub from "../assets/moviehub.png";
 import pizzasi from "../assets/pizzasi.png";
@@ -6,27 +5,6 @@ import pizzasi from "../assets/pizzasi.png";
 export const projectsData = [
   {
     id: 1,
-    name: "Tenzies Game",
-    description:
-      "A dice game where you match 10 dice by holding and rolling until all match.",
-    logos: [
-      {
-        src: "https://www.cdnlogo.com/logos/r/63/react.svg",
-      },
-      {
-        src: "https://www.cdnlogo.com/logos/c/18/css.svg",
-      },
-      {
-        src: "https://www.cdnlogo.com/logos/a/92/android.svg",
-      },
-    ],
-    image: tenzies,
-    github: "https://github.com/kingmathers92/React-Tenzies-Game",
-    live: "https://kingmathers92.github.io/React-Tenzies-Game/",
-    date: "2022-06-03",
-  },
-  {
-    id: 2,
     name: "Crypto Monitor",
     description:
       "Track cryptocurrency stats and prices using CoinGecko API with Firebase for authentication.",
@@ -50,7 +28,7 @@ export const projectsData = [
     date: "2023-01-19",
   },
   {
-    id: 3,
+    id: 2,
     name: "Moviehub",
     description:
       "Moviehub uses React, Redux, and Alan AI to provide advanced movie search capabilities.",
@@ -71,7 +49,7 @@ export const projectsData = [
     date: "2023-03-14",
   },
   {
-    id: 4,
+    id: 3,
     name: "PizzaSi",
     description:
       "A pizza ordering web app designed to assist a local Italian restaurant's business.",
